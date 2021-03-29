c8fcdfa87e2f37313674e6a029a3ff8a22f44b4d
sathesh M <78292248+manisathesh@users.noreply.github.com>
2021-03-29 16:08:52 +0530

    file created
    sample program

---
+++ Amstrong.java
@@ -0,0 +1,40 @@
+package com.basics;
+
+import java.util.Scanner;
+class Perform
+{
+	int val2;
+
+	Perform (int val1)
+	{
+		val2=val1;
+	}
+	
+	void Run() 
+	{
+		int cp=val2;
+		int temp=0;
+		while(val2>0)
+		{
+			int t=val2%10;
+			  temp=temp+(t*t*t);
+			 val2=val2/10;
+		}
+		String s=temp==cp?"Amstrong":"Not an Amstrong";
+		System.out.println(s);
+	}
+	}
+
+public class Amstrong
+{
+public static void main(String[] args)
+{
+Scanner scn=new Scanner(System.in);
+System.out.println("Enter the value");
+int val=scn.nextInt();
+Perform rep=new Perform(val);
+rep.Run();
+
+
+}
+}
